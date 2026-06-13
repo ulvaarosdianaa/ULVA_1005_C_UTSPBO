50a9d698cd71c3e44947f1c15fb0f73d8d2ef534
ulvaarosdianaa <252410101005@mail.unej.ac.id>
2026-06-13 12:55:44 +0700

    UTS PBO TIPE C

---
+++ Class1.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ULVA_1005_C_UTSPBO
+{
+    internal class Class1
+    {
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,82 +1,109 @@
-﻿
-using System;
+﻿using System;
+using System.Collections.Generic;
 
-namespace BioskopOOPCinema
+
+abstract class TiketBioskop
 {
-    public class TiketBioskop
+   
+    private string namaPenonton;
+    private string idBooking;
+    private string judulFilm;
+
+   
+    public string NamaPenonton { get => namaPenonton; set => namaPenonton = value; }
+    public string IdBooking { get => idBooking; set => idBooking = value; }
+    public string JudulFilm { get => judulFilm; set => judulFilm = value; }
+
+
+    public TiketBioskop(string nama, string id, string film)
     {
-        public string namaPenonton { get; set; }
-        public int idBooking { get; set; }
-        public string judulFilm { get; set; }
-
-        public virtual double HitungTotalHarga()
-        {
-            return 0; 
-        }
-
-        public virtual void TampilkanInfo()
-        {
-            Console.WriteLine($"Nama Penonton: {namaPenonton}, id Booking: {idBooking}, judul Film: {judulFilm}");
-        }
+        NamaPenonton = nama;
+        IdBooking = id;
+        JudulFilm = film;
     }
 
-    
-    public class TiketReguler : TiketBioskop
+
+    public void TampilInfo()
     {
-        public double hargaTiket { get; set; }
+        Console.WriteLine($"Nama: {NamaPenonton} | ID: {IdBooking} | Film: {JudulFilm}");
+    }
+
+
+    public abstract int HitungTotalHarga(int jumlahTiket);
+}
+
+
+class TiketReguler : TiketBioskop
+{
+    private int hargaTiket;
+
+    public int HargaTiket { get => hargaTiket; set => hargaTiket = value; }
+
+    public TiketReguler(string nama, string id, string film, int harga)
+        : base(nama, id, film)
+    {
+        HargaTiket = harga;
+    }
+
   
-        public override double HitungTotalHarga()
-        {
-            return (jumlahTiket * hargaTiket);
-        }
-
-        public override void TampilkanInfo()
-        {
-            base.TampilkanInfo();
-            Console.WriteLine($"Harga Tiket: {hargaTiket}");
-        }
-    }
-    public class TiketPremiere : TiketBioskop
+    public override int HitungTotalHarga(int jumlahTiket)
     {
-        public double hargaTiket { get; set; }
-        public double biayaLounge { get; set; }
-
-        public override double hitungTotalHarga()
-        {
-            return (jumlahTiket * hargaTiket) + (biayaLounge);
-        }
-
-        public override void TampilkanInfo()
-        {
-            base.TampilkanInfo();
-            Console.WriteLine($"Harga Tiket: {hargaTiket}, Biaya Lounge: {biayaLounge}");
-        }
-    }
-
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            TiketPremiere[] daftarPenonton = new Penonton[3];
-
-            daftarPenonton[0] = new Penonton
-            {
-                namaPenonton = "Andi",
-                idBooking = "BKS02",
-                jumlahTiket = 3,
-                hargaTiket = 250000,
-                
-            };
-
-            foreach (var Penonton in daftarPenonton)
-            {
-                Penonton.TampilkanInfo();
-                Console.WriteLine($"Total: {Penonton.hitungTotalHarga()}");
-                Console.WriteLine("-----------------------------------");
-            }
-
-            Console.ReadLine();
-        }
+        return jumlahTiket * HargaTiket;
     }
 }
 
+
+class TiketPremiere : TiketBioskop
+{
+    private int hargaTiket;
+    private int biayaLounge;
+
+    public int HargaTiket { get => hargaTiket; set => hargaTiket = value; }
+    public int BiayaLounge { get => biayaLounge; set => biayaLounge = value; }
+
+    public TiketPremiere(string nama, string id, string film, int harga, int lounge)
+        : base(nama, id, film)
+    {
+        HargaTiket = harga;
+        BiayaLounge = lounge;
+    }
+
+    
+    public override int HitungTotalHarga(int jumlahTiket)
+    {
+        return (jumlahTiket * HargaTiket) + BiayaLounge;
+    }
+}
+
+
+class RiwayatNonton
+{
+    public string JenisStudio { get; set; }
+    public int JumlahTiket { get; set; }
+    public string TanggalNonton { get; set; }
+
+    public void CetakRiwayat()
+    {
+        Console.WriteLine($"{JenisStudio} | {JumlahTiket} tiket | {TanggalNonton}");
+    }
+}
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        TiketPremiere tiket1 = new TiketPremiere("Andi", "BKS02", "Spider-Man", 80000, 10000);
+        tiket1.TampilInfo();
+        int total = tiket1.HitungTotalHarga(3);
+        Console.WriteLine($"Total: Rp {total}");
+
+        RiwayatNonton riwayat = new RiwayatNonton
+        {
+            JenisStudio = "Premiere",
+            JumlahTiket = 3,
+            TanggalNonton = "12-10-2025"
+        };
+        riwayat.CetakRiwayat();
+    }
+}
